8d3dbdb282f2fd75ca143eabf6c6fad44d243fc2
Anton <example@gmail.com>
2021-02-19 17:35:43 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// alert('Hello');
+
+
+//  const result = confirm("Are you here?");
+//  console.log(result);
+
+// const answer = +prompt("Вам есть 18?","18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}`);
+
+// const user = "Ivan";
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+
+ 

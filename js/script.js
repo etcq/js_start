71d1451d7b5b8a3e1824e9f9fdb1a6f2f05aa6f4
Anton <example@gmail.com>
2021-02-25 01:05:49 +0500

    after 27 lesson

--- js/script.js
+++ js/script.js
@@ -390,3 +390,103 @@
 //console.log(jonh.armor);
 // jonh.sayHello();
 
+//                  LESSON 25 Breakpoints           //
+
+// function hello() {
+//     console.log('Hello world');
+// }
+
+// hello();
+
+// function hi() {
+//     console.log('Say hi!');
+// }
+
+// hi();
+
+// const arr = [1, 14, 4, 30, 54],
+//       sorted = arr.sort(compareNum);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// console.log(sorted);
+
+//                          Lesson 26 Динамическая типизация            //
+
+// To string
+
+//1)
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+
+//2)
+// console.log(typeof(5 + ''));
+
+// const num = 5;
+
+// console.log("https://vk.com/catalog/" + num);
+
+// const fontSize = 26 + 'px';
+
+//To Number 
+
+//1)
+// console.log(typeof(Number('4')));
+
+//2)
+// console.log(typeof(+'5'));
+//3)
+// console.log(typeof(parseInt("15px", 10)));
+
+// To boolean 
+
+// 0, '', null, undefined, NaN;
+
+//1)
+// let switcher = null;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log('Working...');
+// }
+
+//2)
+// console.log(typeof(Boolean('4')));
+
+//3)
+// console.log(typeof(!!'444444'));
+
+//                                  Lesson 27 Задачи с собеседований                //
+//let x = 5; alert( ++x ); 
+//[ ] + false - null + true 
+//console.log([] + false - null + true); //NaN
+
+//let y = 1; 
+//let x = y = 2; 
+//alert(x); 
+
+//console.log([] + 1 + 2); //12
+
+//alert( "1"[0] );
+
+//console.log(2 && 1 && null && 0 && undefined );
+// И запинается на лжи
+// ИЛИ запинается на правде
+
+// console.log(!!( 1 && 2 ) === (1 && 2));
+//alert( null || 2 && 3 || 4 );
+
+// const a = [1, 2, 3]; 
+// const b = [1, 2, 3]; 
+
+//alert( +"Infinity" ); 
+//console.log("Eжик" > "яблоко");
+
+//console.log(0 || "" || 2 || undefined || true || falsе );
